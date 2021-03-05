cba9043274cdc2cb9ce8067f49e0c87f77e1428a
BabaTee <bossbabatee@gmail.com>
2021-03-05 13:59:28 +0100

    Added login and signup templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return "Welcome to stock project"
+
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template('sign-up.html')
