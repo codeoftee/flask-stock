62367cb2a44be9904464a0a7ce759a1f001ec82d
BabaTee <bossbabatee@gmail.com>
2021-03-26 10:43:21 +0100

    Added product models

--- app/models.py
+++ app/models.py
@@ -10,3 +10,15 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User id {}, name {}>'.format(self.id, self.name)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100))
+    price = db.Column(db.NUMERIC(10, 2))
+    compare_price = db.Column(db.NUMERIC(10, 2))
+    description = db.Column(db.Text)
+    pictures = db.Column(db.Text)
+    tags = db.Column(db.String(200))
+    category = db.Column(db.String(200))
+    added = db.Column(db.DateTime)
